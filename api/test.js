43c0b36f7c399e325141aebe789399d0add3468f
Yoda <yoda@yodacode.xyz>
2023-02-13 21:43:37 -0800

    test

--- api/test.js
+++ api/test.js
@@ -2,5 +2,5 @@ export default function (req, res) {
 
     console.log({ req, res });
 
-    res.status(200).json({ name: 'John Doe' });
+    res.status(200).json({ name: 'John Doe', req, res });
 }
