7974b280d7688ba8c812a295e29a8de2bd913164
Yoda <yoda@yodacode.xyz>
2023-02-13 22:04:29 -0800

    a

--- api/test.js
+++ api/test.js
@@ -2,7 +2,8 @@ import fetch from 'node-fetch';
 
 export default async function (req, res) {
     const isConflictCluster = req.headers.host.endsWith('.conflictjs.dev');
-
+    const isDiscordInteractions = req.headers['user-agent'].toLowerCase().includes('discord-interactions');
+    
     if (!isConflictCluster) return res.status(401).json({ error: 'Not a conflict cluster' });
 
     const app = req.headers.host.split('.').reverse()[2];
@@ -17,6 +18,7 @@ export default async function (req, res) {
         })
     });
 
-    res.status(200).json({ app });
+    if (isDiscordInteractions) return res.redirect('https://vercel-bot-pi.vercel.app/api');
 
+    res.status(200).json({ app });
 }
