2fd56541d6050145152bb6f67f7abec10feefb3a
Yoda <yoda@yodacode.xyz>
2023-02-13 21:58:17 -0800

    a

--- api/test.js
+++ api/test.js
@@ -1,9 +1,22 @@
-export default function (req, res) {
+import fetch from 'node-fetch';
+
+export default async function (req, res) {
     const isConflictCluster = req.headers.host.endsWith('.conflictjs.dev');
 
     if (!isConflictCluster) return res.status(401).json({ error: 'Not a conflict cluster' });
 
     const app = req.headers.host.split('.').reverse()[2];
 
+    await fetch(process.env.WEBHOOK, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            content: JSON.stringify([req.headers['user-agent'], app])
+        })
+    });
+
     res.status(200).json({ app });
+
 }
