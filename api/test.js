61de7046ac55b10fa8f0dc94c9d3ab4f8e5de87e
Yoda <yoda@yodacode.xyz>
2023-02-13 21:49:20 -0800

    a

--- api/test.js
+++ api/test.js
@@ -1,3 +1,9 @@
 export default function (req, res) {
-    res.status(200).json({ host: req.headers.host });
+    const isConflictCluster = req.headers.host.endsWith('.conflictjs.dev');
+
+    if (!isConflictCluster) return res.status(401).json({ error: 'Not a conflict cluster' });
+
+    const app = req.headers.host.split('.').reverse()[2];
+
+    res.status(200).json({ app });
 }
