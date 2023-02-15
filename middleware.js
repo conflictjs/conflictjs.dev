deb7fd21e6e7fe1da82602ae6c7fe69805e29d97
Yoda <yoda@yodacode.xyz>
2023-02-15 14:48:19 -0800

    a

--- middleware.js
+++ middleware.js
@@ -5,6 +5,8 @@ export const config = {
 }
 
 export default function middleware(request) {
+    console.log(request.host)
+
     if (request.headers.get('user-agent')?.toLowerCase?.()?.includes?.('discord')) return rewrite(new URL('https://vercel-conflict-bot.vercel.app/discord', request.url));
 
     return rewrite(new URL('/dashboard.html', request.url));
