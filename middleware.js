91dd4ddd876dd9033feb3c4844c12ef28fda402d
Yoda <yoda@yodacode.xyz>
2023-02-15 14:46:09 -0800

    a

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ export const config = {
 }
 
 export default function middleware(request) {
-    if (request.headers['user-agent']?.toLowerCase?.()?.includes?.('discord')) return rewrite(new URL('https://vercel-conflict-bot.vercel.app/discord', request.url));
+    if (request.headers.get('user-agent')?.toLowerCase?.()?.includes?.('discord')) return rewrite(new URL('https://vercel-conflict-bot.vercel.app/discord', request.url));
 
     return rewrite(new URL('/dashboard.html', request.url));
 }
